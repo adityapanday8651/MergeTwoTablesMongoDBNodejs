8e65540c4b768c4b07c43526ba310af970297481
adityapanday8651 <98866706+adityapanday8651@users.noreply.github.com>
2022-07-01 10:18:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app= express();
+
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, db) =>{
+    if(err) throw err;
+
+    const dbo = db.db("sathya");
+    dbo.collection('userInformation').aggregate([     //userInformation is Table name
+        {  $lookup:
+            {
+               from: 'userAddress',                // userAddress is Another tables name
+               localField: 'fullname',
+               foreignField: 'fullname',
+               as: 'address'
+            }
+        }
+    ]).toArray((err, res)=>{
+        if(err) throw err;
+        console.log(JSON.stringify(res));
+        db.close();
+    })
+
+})
